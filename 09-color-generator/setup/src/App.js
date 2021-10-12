5b0e382ec5d2f0613afd2d13d2e2f8d4f49f12b8
tejan-singh <tstejan@gmail.com>
2021-10-12 17:48:21 +0530

    Feat: display color palette with hex code

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,43 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('')
+  const [list, setList] = useState([])
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+      console.log(list)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e) => {
+    setColor(e.target.value)
+  }
+
+  return <>
+    <section className='container'>
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='#f15025' onChange={handleChange} className={error?"error":null}/>
+        <button type='submit' className='btn'>Submit</button>
+      </form>
+    </section>
+    <section className='colors'>
+    {list.map((eachColor, index) => {
+      //pass all properties as props without extracting each
+      //note: not able to access hex property from eachColor. So, we passed it as props by accessing it from object
+      return <SingleColor index = {index} {...eachColor} key={index} hexCode={eachColor.hex}/>
+    })}
+    </section>
+
+  </>
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index, hexCode}) => {
+  //this will convert rgb arrary values to a integer seperated by a comma
+  const bcg = rgb.join(',')
+  // console.log(bcg)
+
+  //another way to pass hexCode is to convert array of rgb value to hex using a rgbToHex function
+  // to generate array of hexCode we pass rgb array with spread operator. This will generate an array of converted hexCode
+  // const hexCode = rgbToHex(...rgb)
+
+  // set background value of each color according to the rgb value got from bcg
+  return (
+    <article className='color' style={{backgroundColor: `rgb(${bcg})`}}>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexCode}</p>
+  </article>
+  )
 }
 
 export default SingleColor
