00d283a0e0138223b3baccc0c5de0401c4d48123
tejan-singh <tstejan@gmail.com>
2021-10-14 17:21:39 +0530

    implemented toggle nav functionality using state

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,33 +4,37 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={()=>{setShowLinks(!showLinks)}}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            {links.map((link) => {
-              return (
-                <li key={link.id}>
-                  <a href={link.url}>{link.text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+
+        {showLinks && (
+          <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link) => {
+                return (
+                  <li key={link.id}>
+                    <a href={link.url}>{link.text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+
         <ul className="social-icons">
           {social.map((social) => {
             return (
               <li key={social.id}>
-                <a href={social.url}>
-                  {social.icon}
-                </a>
+                <a href={social.url}>{social.icon}</a>
               </li>
             );
           })}
