bb23c16f67f5a25d1c5849af57e9d28ae680aefe
tejan-singh <tstejan@gmail.com>
2021-10-19 19:09:40 +0530

    added context to modal.js

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { FaTimes } from 'react-icons/fa'
 import { AppContext } from './context'
 
 const Modal = () => {
-  return <div className="modal-overlay">
+  const {isModelOpen, closeModel} = useContext(AppContext)
+
+  return <div className={isModelOpen ? "modal-overlay show-modal": "modal-overlay"}>
     <div className="modal-container">
       <h3>model content</h3>
-      <button className="close-modal-btn">
+      <button className="close-modal-btn" onClick={closeModel}>
         <FaTimes/>
       </button>
     </div>
