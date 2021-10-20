350d51af756f63ab13c7132c3b9c702729fdd64b
tejan-singh <tstejan@gmail.com>
2021-10-20 14:00:49 +0530

    added context to index.js

--- 13-stripe-submenus/setup/src/App.js
+++ 13-stripe-submenus/setup/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
 import Sidebar from './Sidebar'
 import Submenu from './Submenu'
+import { AppContext } from './context'
 function App() {
+  const data = useContext(AppContext)
+  
   return (
     <>
-      <h2>stripe submenus setup</h2>
+      <Navbar/>
+      <Sidebar/>
+      <Hero/>
+      <Submenu/>
     </>
   )
 }
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,13 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value={'hello'}>
+        {children}
+    </AppContext.Provider>
+}
+
+export {AppContext, AppProvider}
