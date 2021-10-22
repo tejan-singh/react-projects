b350e551cbe6c7651cab6c23e6375faadd101cc2
tejan-singh <tstejan@gmail.com>
2021-10-22 16:59:44 +0530

    fix: negative amount value by filter method

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,36 +1,44 @@
 // here state is a state before component update
 const reducer = (state, action) => {
-    if(action.type === 'CLEAR_CART'){
-        return {...state, cart: []}
-    }
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
 
-    if(action.type === 'REMOVE_ITEM'){
-        let newList = state.cart.filter((cartItem) => {return (cartItem.id !== action.payload)})
-        return {...state, cart:newList}
-    }
+  if (action.type === "REMOVE_ITEM") {
+    let newList = state.cart.filter((cartItem) => {
+      return cartItem.id !== action.payload;
+    });
+    return { ...state, cart: newList };
+  }
 
-    if(action.type === 'INCREASE'){
-        let newCart = state.cart.map((cartItem) => {
-            if (cartItem.id === action.payload){
-                return {...cartItem, amount: cartItem.amount + 1}
-            }
+  if (action.type === "INCREASE") {
+    let newCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
 
-            return cartItem
-        })
-        return {...state, cart:newCart}
-    }
+      return cartItem;
+    });
+    return { ...state, cart: newCart };
+  }
 
-    if(action.type === 'DECREASE'){
-        let newCart = state.cart.map((cartItem) => {
-            if (cartItem.id === action.payload){
-                return {...cartItem, amount: cartItem.amount - 1}
-            }
-            return cartItem
-        })
-        return {...state, cart:newCart}
-    }
+  if (action.type === "DECREASE") {
+    let newCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      //chaining filter with map method -> to remove item when amount is less then 0
+      .filter((cartItem) => {
+        return cartItem.amount !== 0;
+      });
 
-    return state
-}
+    return { ...state, cart: newCart };
+  }
 
-export default reducer
+  return state;
+};
+
+export default reducer;
